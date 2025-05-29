5eedef5c93e51c5c81078dde9709c71afedc88b0
Lucas Lopez Montero <llopezmo@supportshell-1.sush-001.prod.eu-central-1.aws.redhat.com>
2025-05-29 11:36:32 +0000

    echo replaced with printf

--- lsos.sh
+++ lsos.sh
@@ -28,16 +28,6 @@ readonly NO_COLOUR='\033[0m'
 export SYSTEMD_COLORS=false
 
 
-<<<<<<< HEAD
-echo_title()
-	{
-	local i
-	local UNDERLINE_CHAR='-'
-	if [ $# -ne 1 ]
-		then
-		echo "Error: function called with missing argument" >&2
-=======
->>>>>>> printf
 printf_title()
 	{
 	local i
@@ -49,28 +39,6 @@ printf_title()
 		fi
 	local STR="$1"
 	local LENGTH=${#STR}
-<<<<<<< HEAD
-	echo -e "${RED}${STR}"
-	for i in $(seq 1 $LENGTH)
-		do
-		echo -en "$UNDERLINE_CHAR"
-		done
-	echo -e "${NO_COLOUR}"
-	}
-
-echo_green()
-	{
-	if [ $# -ne 1 ]
-                then
-                echo "Error: function called with missing argument" >&2
-                return 22
-                fi
-        local STR="$1"
-	echo -e "${GREEN}${STR}${NO_COLOUR}"
-	}
-
-=======
->>>>>>> printf
 	printf "${RED}${STR}\n"
 	for i in $(seq 1 $LENGTH)
 		do
@@ -112,17 +80,6 @@ cd $1
 
 
 # SYSTEM
-<<<<<<< HEAD
-echo_title "SYSTEM"
-echo_green "\nOperating system:"
-grep -w "^PRETTY_NAME" etc/os-release | awk -F '=' '{print $2}' | tr -d '"'
-echo_green "\nKernel:"
-cat 'uname'
-echo -e "\n"
-
-# CPU
-echo_title "CPU"
-echo_green "\nCPU load:"
 printf_title "SYSTEM"
 printf_green "\nOperating system:"
 grep -w "^PRETTY_NAME" etc/os-release | awk -F '=' '{print $2}' | tr -d '"'
@@ -131,27 +88,12 @@ cat 'uname'
 printf "\n\n"
 
 # CPU
-=======
-printf_title "SYSTEM"
-printf_green "\nOperating system:"
-grep -w "^PRETTY_NAME" etc/os-release | awk -F '=' '{print $2}' | tr -d '"'
-printf_green "\nKernel:"
-cat 'uname'
-printf "\n\n"
-
-# CPU
->>>>>>> printf
 printf_title "CPU"
 printf_green "\nCPU load:"
 cat 'uptime'
 CORES=$(grep '^processor' proc/cpuinfo | tail -n 1 | awk '{print $3}')
 # The core count in proc/cpuinfo starts with zero. Therefore, adding 1
 CORES=$((${CORES}+1))
-<<<<<<< HEAD
-echo "Number of cores: $CORES"
-echo_green "\nTop CPU-consuming process/es:"
-=======
->>>>>>> printf
 printf "Number of cores: ${CORES}\n"
 printf_green "\nTop CPU-consuming process/es:"
 head -n 1 ps
@@ -160,27 +102,11 @@ head -n 1 ps
 printf "\n\n"
 
 # Memory
-<<<<<<< HEAD
-echo_title "MEMORY"
-echo_green "\nMemory load in MiB:"
-cat 'sos_commands/memory/free_-m'
-USED_MEMORY=$(grep -w '^Mem:' 'sos_commands/memory/free_-m' | awk '{print $3}')
-TOTAL_MEMORY=$(grep -w '^Mem:' 'sos_commands/memory/free_-m' | awk '{print $2}')
-USED_MEMORY_PERCENT=$(echo "scale=10; $USED_MEMORY / $TOTAL_MEMORY * 100" | bc -l)
-printf "Memory use: %.2f%% \n" $USED_MEMORY_PERCENT
-echo_green "\nTop memory-consuming process/es:"
 printf_title "MEMORY"
 printf_green "\nMemory load in MiB:"
 cat 'sos_commands/memory/free_-m'
 USED_MEMORY=$(grep -w '^Mem:' 'sos_commands/memory/free_-m' | awk '{print $3}')
 TOTAL_MEMORY=$(grep -w '^Mem:' 'sos_commands/memory/free_-m' | awk '{print $2}')
-=======
-printf_title "MEMORY"
-printf_green "\nMemory load in MiB:"
-cat 'sos_commands/memory/free_-m'
-USED_MEMORY=$(grep -w '^Mem:' 'sos_commands/memory/free_-m' | awk '{print $3}')
-TOTAL_MEMORY=$(grep -w '^Mem:' 'sos_commands/memory/free_-m' | awk '{print $2}')
->>>>>>> printf
 USED_MEMORY_PERCENT=$(echo "scale=10; $USED_MEMORY / $TOTAL_MEMORY * 100" | bc --mathlib)
 printf "Memory use: "
 if (( $(echo "$USED_MEMORY_PERCENT >= $MEMORY_WARNING" | bc --mathlib) ))
@@ -196,11 +122,6 @@ head -n 1 ps
 printf "\n\n"
 
 # Local storage
-<<<<<<< HEAD
-echo_title "LOCAL STORAGE"
-echo_green "\nMost relevant file systems:"
-=======
->>>>>>> printf
 printf_title "LOCAL STORAGE"
 printf_green "\nMost relevant file systems:"
 grep -w '/' sos_commands/filesys/df_-al_-x_autofs
@@ -212,55 +133,22 @@ if ! grep -w '/var' sos_commands/filesys/df_-al_-x_autofs | grep -Fv '/var/'
 printf "\n\n"
 
 # Logs
-<<<<<<< HEAD
-echo_title "LOGS"
-JOURNAL_FIND=$(find ./var -name system.journal)
-if [ $(echo "$JOURNAL_FIND" | wc -l) -gt 1 ]
-	then
-	echo "Error: multiple system.journal files found in var directory:" >&2
-	echo "$JOURNAL_FIND" >&2
 printf_title "LOGS"
 JOURNAL_FIND=$(find ./var -name system.journal)
 if [ $(printf "$JOURNAL_FIND" | wc -l) -gt 1 ]
 	then
-=======
-printf_title "LOGS"
-JOURNAL_FIND=$(find ./var -name system.journal)
-if [ $(printf "$JOURNAL_FIND" | wc -l) -gt 1 ]
-	then
->>>>>>> printf
 	printf "Error: multiple system.journal files found in var directory:\n" >&2
 	printf "${JOURNAL_FIND}\n" >&2
 	exit 7
 	fi
 if [ -z "$JOURNAL_FIND" ] || [ $(printf "$JOURNAL_FIND" | wc -l) -eq 0 ]
 	then
-<<<<<<< HEAD
-	echo "Error: No system.journal file found in var directory" >&2
-=======
->>>>>>> printf
 	printf "Error: No system.journal file found in var directory\n" >&2
 	exit 7
 	fi
 JOURNAL_DIR=$(readlink -e "$(dirname "$JOURNAL_FIND")")
 #LOG_LINES=$(journalctl -D "$JOURNAL_DIR" --no-pager | wc -l)
 LOG_LINES_ERR=$(journalctl -D "$JOURNAL_DIR" --no-pager -p err | wc -l)
-<<<<<<< HEAD
-#LOG_ERR_PERCENTAGE=$(echo "scale=10; $LOG_LINES_ERR / $LOG_LINES * 100" | bc -l)
-echo_green "\nNumber of error log lines:"
-echo "Total: $LOG_LINES_ERR"
-#printf "Percentage: %.1f%% \n" $LOG_ERR_PERCENTAGE
-echo_green "\nLast error log line/s:"
-journalctl -D "$JOURNAL_DIR" --no-pager -p err -n $LOG_LENGTH 
-echo_green "\nLast error log line/s of crio unit:"
-journalctl -D "$JOURNAL_DIR" --no-pager -u crio -p err -n $LOG_LENGTH 
-echo_green "\nLast error log line/s of kubelet unit:"
-journalctl -D "$JOURNAL_DIR" --no-pager -u kubelet -p err -n $LOG_LENGTH 
-echo_green "\nLast error log line/s of kernel:"
-journalctl -D "$JOURNAL_DIR" --no-pager -t kernel -p err -n $LOG_LENGTH 
-echo_green "\nCommands to execute for complete logs:"
-=======
->>>>>>> printf
 #LOG_ERR_PERCENTAGE=$(printf "scale=10; $LOG_LINES_ERR / $LOG_LINES * 100" | bc -l)
 printf_green "\nNumber of error log lines:"
 printf "Total: ${LOG_LINES_ERR}\n"
